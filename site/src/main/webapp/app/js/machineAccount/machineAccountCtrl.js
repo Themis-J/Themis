723d1076bb7ef8736feda4cb383166e37f36ff85
xduo <oranjedog@gmail.com>
2014-04-07 17:14:15 +0800

    add pagination

--- site/src/main/webapp/app/js/machineAccount/machineAccountCtrl.js
+++ site/src/main/webapp/app/js/machineAccount/machineAccountCtrl.js
@@ -11,7 +11,6 @@ angular.module('commonControllers', []).
             $scope.type = "vehicle";
             $scope.typeString = "新车销售台账";
 
-            $scope.loadingList = false;
             $scope.metadataStore = {};
             $scope.metadata = {};
             $scope.data = {};
@@ -23,6 +22,12 @@ angular.module('commonControllers', []).
             $scope.postQuery = {};
             $scope.dealers = {};
             $scope.dealer = {};
+            $scope.state = {
+                listType: '',
+                loading: false,
+                hasMore: false,
+                perPage: 10
+            };
             $scope.header = 'partials/branch/header.html';
 
             if (UserService.validateRoleHead()) {
@@ -31,7 +36,6 @@ angular.module('commonControllers', []).
 
             var dealers = Dealer.list({}, function () {
                 $scope.dealers = dealers.items;
-
                 $scope.dealer = $scope.dealers[0];
             });
 
@@ -39,33 +43,98 @@ angular.module('commonControllers', []).
                 if (type) {
                     $scope.type = type;
                 }
-                var dealderId = null;
+                var dealerID = null;
                 if ($scope.dealer.id) {
-                    dealderId = $scope.dealer.id;
+                    dealerID = $scope.dealer.id;
                 }
                 else {
-                    dealderId = DealerService.getDealerId();
+                    dealerID = DealerService.getDealerId();
                 }
 
+                $scope.state.listType = 'normal';
                 if ($scope.type == "vehicle") {
                     $scope.list = [];
-                    $scope.loadingList = true;
-                    machineAccountService.queryVehicleSalesLedgerData({dealerID: dealderId},
+                    $scope.state.loading = true;
+                    machineAccountService.queryVehicleSalesLedgerData({dealerID: dealerID, marker: $scope.list.length, limit: $scope.state.perPage},
                         function (data) {
-                            $scope.loadingList = false;
+                            $scope.state.loading = false;
+                            $scope.state.hasMore = (data.summaries.length == $scope.state.perPage) ? true : false;
                             $scope.list = data.summaries;
                         });
                 }
                 if ($scope.type == "postSales") {
                     $scope.list = [];
-                    $scope.loadingList = false;
-                    machineAccountService.queryPostSalesLedgerData({dealerID: dealderId},
+                    $scope.loading = true;
+                    machineAccountService.queryPostSalesLedgerData({dealerID: dealerID, marker: $scope.list.length, limit: $scope.state.perPage},
                         function (data) {
-                            $scope.loadingList = false;
+                            $scope.loading = false;
+                            $scope.state.hasMore = (data.summaries.length == $scope.state.perPage) ? true : false;
                             $scope.list = data.summaries;
                         });
                 }
                 $scope.panel = "partials/common/machineAccount/list.html";
+            }
+
+            $scope.showMore = function () {
+                var dealerID = null;
+                if ($scope.dealer.id) {
+                    dealerID = $scope.dealer.id;
+                }
+                else {
+                    dealerID = DealerService.getDealerId();
+                }
+
+                if ('normal' == $scope.state.listType) {
+                    if ($scope.type == "vehicle") {
+                        $scope.state.loading = true;
+                        machineAccountService.queryVehicleSalesLedgerData({dealerID: dealerID, marker: $scope.list.length, limit: $scope.state.perPage},
+                            function (data) {
+                                $scope.state.loading = false;
+                                $scope.state.hasMore = (data.summaries.length == $scope.state.perPage) ? true : false;
+                                $scope.list = $scope.list.concat(data.summaries);
+                            });
+                    }
+                    if ($scope.type == "postSales") {
+                        $scope.state.loading = true;
+                        machineAccountService.queryPostSalesLedgerData({dealerID: dealerID, marker: $scope.list.length, limit: $scope.state.perPage},
+                            function (data) {
+                                $scope.state.loading = false;
+                                $scope.state.hasMore = (data.summaries.length == $scope.state.perPage) ? true : false;
+                                $scope.list = $scope.list.concat(data.summaries);
+                            });
+                    }
+                }
+                else {
+                    if ('queryV' == $scope.state.listType) {
+                        $scope.state.loading = true;
+                        machineAccountService.queryVehicleSalesLedgerData({contractNo: $scope.vehicleQuery.contractNo,
+                                model: $scope.vehicleQuery.model, type: $scope.vehicleQuery.type, color: $scope.vehicleQuery.color,
+                                lpNumber: $scope.vehicleQuery.lpNumber, frameNo: $scope.vehicleQuery.frameNo, manufacturerDebitDate: $scope.vehicleQuery.manufacturerDebitDate,
+                                warehousingDate: $scope.vehicleQuery.warehousingDate, salesDate: $scope.vehicleQuery.salesDate, guidingPrice: $scope.vehicleQuery.guidingPrice,
+                                customerName: $scope.vehicleQuery.customerName, identificationNo: $scope.vehicleQuery.identificationNo, salesConsultant: $scope.vehicleQuery.salesConsultant,
+                                customerType: $scope.vehicleQuery.customerType, dealerID: dealerID, marker: $scope.list.length, limit: $scope.state.perPage},
+                            function (data) {
+                                $scope.state.loading = false;
+                                $scope.state.hasMore = (data.summaries.length == $scope.state.perPage) ? true : false;
+                                $scope.list = $scope.list.concat(data.summaries);
+                            });
+                    }
+
+                    if ('queryP' == $scope.state.listType) {
+                        $scope.state.loading = true;
+                        machineAccountService.queryPostSalesLedgerData({workOrderNo: $scope.postQuery.workOrderNo, salesDate: $scope.postQuery.salesDate,
+                                mileage: $scope.postQuery.mileage, lpNumber: $scope.postQuery.lpNumber, customerName: $scope.postQuery.customerName,
+                                color: $scope.postQuery.color, frameNo: $scope.postQuery.frameNo, model: $scope.postQuery.model, enterFactoryDate: $scope.postQuery.enterFactoryDate,
+                                exitFactoryDate: $scope.postQuery.exitFactoryDate, customerType: $scope.postQuery.customerType, insuranceAgengcy: $scope.postQuery.insuranceAgengcy,
+                                insuranceDueDate: $scope.postQuery.insuranceDueDate, insuranceClaimNumber: $scope.postQuery.insuranceClaimNumber, dealerID: dealerID,
+                                marker: $scope.list.length, limit: $scope.state.perPage},
+                            function (data) {
+                                $scope.state.loading = false;
+                                $scope.state.hasMore = (data.summaries.length == $scope.state.perPage) ? true : false;
+                                $scope.list = $scope.list.concat(data.summaries);
+                            });
+                    }
+                }
             }
 
             if ($routeParams.id) {
@@ -124,34 +193,55 @@ angular.module('commonControllers', []).
             });
 
             $scope.queryVehices = function () {
+                var dealerID = null;
+                if ($scope.dealer.id) {
+                    dealerID = $scope.dealer.id;
+                }
+                else {
+                    dealerID = DealerService.getDealerId();
+                }
+                $scope.state.loading = true;
+                $scope.state.listType = 'queryV';
                 $scope.list = [];
                 machineAccountService.queryVehicleSalesLedgerData({contractNo: $scope.vehicleQuery.contractNo,
                         model: $scope.vehicleQuery.model, type: $scope.vehicleQuery.type, color: $scope.vehicleQuery.color,
                         lpNumber: $scope.vehicleQuery.lpNumber, frameNo: $scope.vehicleQuery.frameNo, manufacturerDebitDate: $scope.vehicleQuery.manufacturerDebitDate,
                         warehousingDate: $scope.vehicleQuery.warehousingDate, salesDate: $scope.vehicleQuery.salesDate, guidingPrice: $scope.vehicleQuery.guidingPrice,
                         customerName: $scope.vehicleQuery.customerName, identificationNo: $scope.vehicleQuery.identificationNo, salesConsultant: $scope.vehicleQuery.salesConsultant,
-                        customerType: $scope.vehicleQuery.customerType},
+                        customerType: $scope.vehicleQuery.customerType, dealerID: dealerID, marker: $scope.list.length, limit: $scope.state.perPage},
                     function (data) {
+                        $scope.state.loading = false;
+                        $scope.state.hasMore = (data.summaries.length == $scope.state.perPage) ? true : false;
                         $scope.list = data.summaries;
                     });
 
-                $scope.vehicleQuery = {};
                 $('#search_vehicle_modal').modal('hide');
                 $scope.panel = "partials/common/machineAccount/list.html";
             }
 
             $scope.queryPosts = function () {
+                var dealerID = null;
+                if ($scope.dealer.id) {
+                    dealerID = $scope.dealer.id;
+                }
+                else {
+                    dealerID = DealerService.getDealerId();
+                }
+                $scope.state.loading = true;
+                $scope.state.listType = 'queryP';
                 $scope.list = [];
                 machineAccountService.queryPostSalesLedgerData({workOrderNo: $scope.postQuery.workOrderNo, salesDate: $scope.postQuery.salesDate,
                         mileage: $scope.postQuery.mileage, lpNumber: $scope.postQuery.lpNumber, customerName: $scope.postQuery.customerName,
                         color: $scope.postQuery.color, frameNo: $scope.postQuery.frameNo, model: $scope.postQuery.model, enterFactoryDate: $scope.postQuery.enterFactoryDate,
                         exitFactoryDate: $scope.postQuery.exitFactoryDate, customerType: $scope.postQuery.customerType, insuranceAgengcy: $scope.postQuery.insuranceAgengcy,
-                        insuranceDueDate: $scope.postQuery.insuranceDueDate, insuranceClaimNumber: $scope.postQuery.insuranceClaimNumber},
+                        insuranceDueDate: $scope.postQuery.insuranceDueDate, insuranceClaimNumber: $scope.postQuery.insuranceClaimNumber, dealerID: dealerID,
+                        marker: $scope.list.length, limit: $scope.state.perPage},
                     function (data) {
+                        $scope.state.loading = false;
+                        $scope.state.hasMore = (data.summaries.length == $scope.state.perPage) ? true : false;
                         $scope.list = data.summaries;
                     });
 
-                $scope.postQuery = {};
                 $('#search_post_modal').modal('hide');
                 $scope.panel = "partials/common/machineAccount/list.html";
             }
@@ -190,7 +280,6 @@ angular.module('commonControllers', []).
                     machineAccountService.saveVehicleSalesLedgerData({}, $scope.data, function () {
                         $scope.message.text = "台账信息保存成功!";
                         $scope.message.class = "text-success";
-                        $scope.refreshList();
                     }, function () {
                         $scope.message.text = "台账信息保存失败。";
                         $scope.message.class = "text-warning";
@@ -200,7 +289,6 @@ angular.module('commonControllers', []).
                     machineAccountService.savePostSalesLedgerData({}, $scope.data, function () {
                         $scope.message.text = "台账信息保存成功!";
                         $scope.message.class = "text-success";
-                        $scope.refreshList();
                     }, function () {
                         $scope.message.text = "台账信息保存失败。";
                         $scope.message.class = "text-warning";
@@ -230,4 +318,8 @@ angular.module('commonControllers', []).
                 }
                 return parseFloat(str.replace(/[^\d\.\-]/g, ""));
             }
+        }])
+    .controller('searchMachineAccountCtrl', ['$scope', '$route', '$location', '$routeParams', 'DealerService', 'Dealer', 'machineAccountService', 'UserService', '$timeout',
+        function ($scope, $route, $location, $routeParams, DealerService, Dealer, machineAccountService, UserService, $timeout) {
+
         }]);
